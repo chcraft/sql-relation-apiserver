f6d2e54ebdd91337b8a6c2b5e8bae5a07229229f
박찬하 <chanhacraft@naver.com>
2021-03-02 04:35:00 +0900

    first commit

---
+++ src/main/java/com/chcraft/sqlrelation/SqlRelationApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.chcraft.sqlrelation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlRelationApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlRelationApiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chcraft/sqlrelation/SqlRelationApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chcraft.sqlrelation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlRelationApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
